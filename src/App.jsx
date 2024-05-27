f0bbf8d277a75b62f3b3840d7bc4ed175c89820d
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-05-28 01:04:48 +0530

    remove app.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import "./App.css";
 
 function App() {
   return (
